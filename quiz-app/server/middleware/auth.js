6905922acf8e93635b3bce7b652287d8ab04935c
samirulcodes <samirulislam.official.com>
2024-10-15 00:03:37 +0530

    Quiz-App-FullStack

--- quiz-app/server/middleware/auth.js
+++ quiz-app/server/middleware/auth.js
@@ -1,5 +1,4 @@
 
-
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
--- quiz-app/server/routes/quiz.js
+++ quiz-app/server/routes/quiz.js
@@ -1,43 +1,4 @@
 
-// const express = require('express');
-// const QuizResult = require('../models/QuizResult');
-// const auth = require('../middleware/auth');  // Import authentication middleware
-
-// const router = express.Router();
-
-// // Save quiz result - protected route
-// router.post('/save', auth, async (req, res) => {
-//     const { score, totalQuestions } = req.body;
-
-//     console.log('Received quiz save request');  // Log to confirm the request is hitting the server
-//     console.log('Score received:', score);  // Log score received
-//     console.log('Total Questions received:', totalQuestions);  // Log total questions received
-//     console.log('User ID:', req.user.id);  // Log user ID from the token
-
-//     try {
-//         const quizResult = new QuizResult({
-//             userId: req.user.id,  // Get the logged-in user’s ID from the auth middleware
-//             score,
-//             totalQuestions,
-//         });
-
-//         await quizResult.save();
-//         console.log('Quiz result saved successfully!');  // Log success message
-
-//         res.status(201).json({ message: 'Quiz result saved successfully' });
-//     } catch (error) {
-//         console.error('Error saving quiz result:', error.message);  // Log error details
-//         res.status(500).json({ message: 'Error saving quiz result' });
-//     }
-// });
-
-// module.exports = router;
-
-
-
-
-
-
 const express = require('express');
 const QuizResult = require('../models/QuizResult');
 const auth = require('../middleware/auth');  // Import authentication middleware
